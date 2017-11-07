a0480b38cb2bfa1294c85991c8a536c09fdefd3d
Austin Kurtis <austinkurtis@Austins-MacBook-Pro.local>
2017-11-07 09:42:44 -0600

    working on product update

--- lib/controllers/product.rb
+++ lib/controllers/product.rb
@@ -71,55 +71,135 @@ class Product
         return true
     end
 
-# import_products Pulls all products that are not on an order from the database.    
-# Author: Austin Kuirts    
-def self.import_products
-    products = []
-    db = SQLite3::Database.open("bangazon_store.sqlite")
-    all_products = db.prepare "SELECT * FROM products WHERE product_id NOT IN (SELECT product_id FROM order_products) AND seller_id = #{$ACTIVE_CUSTOMER_ID}"
-    products = all_products
-end
-
-def self.remove_product
-    sleep(0.75)
-    productIds = []
-    products = self.import_products
-    print "\n"
-    products.each do |product_id, price, title| 
-    print "#{product_id}" + ". " + "#{title}\n"
-    productIds.push(product_id)
-    print "\n Choose a Product to delete:".upcase
+    # import_products Pulls all products that are not on an order from the database.    
+    # Author: Austin Kuirts    
+    def self.import_products
+        products = []
+        db = SQLite3::Database.open("bangazon_store.sqlite")
+        all_products = db.prepare "SELECT * FROM products WHERE product_id NOT IN (SELECT product_id FROM order_products) AND seller_id = #{$ACTIVE_CUSTOMER_ID}"
+        products = all_products
     end
+
+    def self.remove_product
+        sleep(0.75)
+        productIds = []
+        products = self.import_products
+        print "\n"
+        products.each do |product_id, price, title| 
+        print "#{product_id}" + ". " + "#{title}\n"
+        productIds.push(product_id)
+        print "\n Type EXIT to return to main menu:\n".upcase
+        print "\n Choose a Product to delete: ".upcase
+        end
         
-    if productIds.empty?
-        print "This customer has no products. Would you like to add a product? Y/N: "
-        selection = gets.upcase.chomp
-       if selection == "Y"
-            self.add_product_to_active_customer
-            elsif selection =="N"
-        else
-            print "Unrecongnized selection".upcase
-            self.remove_product
-       end
-    else
-        product_to_delete = gets.chomp.to_i
-        if productIds.include?(product_to_delete)
-            db = SQLite3::Database.open("bangazon_store.sqlite")
-            db.execute "DELETE FROM products WHERE product_id = #{product_to_delete} AND seller_id = #{$ACTIVE_CUSTOMER_ID} "
-            db.close
-            system "clear" or system "cls"
-        else
-            print "Unrecongnized selection\n".upcase
+        if productIds.empty?
+            print "This customer has no products. Would you like to add a product? Y/N: "
+            selection = gets.upcase.chomp
+        if selection == "Y"
+                self.add_product_to_active_customer
+                elsif selection =="N"
+            else
+                print "Unrecongnized selection".upcase
                 self.remove_product
         end
+        else
+            product_to_delete = gets.chomp
+            if productIds.include?(product_to_delete.to_i)
+                db = SQLite3::Database.open("bangazon_store.sqlite")
+                db.execute "DELETE FROM products WHERE product_id = #{product_to_delete} AND seller_id = #{$ACTIVE_CUSTOMER_ID} "
+                db.close
+                system "clear" or system "cls"
+            elsif product_to_delete.upcase == "EXIT"
 
-    end
+            else
+                print "Unrecongnized selection\n".upcase
+                    self.remove_product
+            end
+
+        end
 
      
-end
+    end
 
 
-    def update_product
+    def self.update_product
+        sleep(0.75)
+        update_productIds = []
+        update_products = self.import_products
+        print "\n"
+        update_products.each do |product_id, price, title| 
+        print "#{product_id}" + ". " + "#{title}\n"
+        update_productIds.push(product_id)
+        print "\n Type EXIT to return to main menu:\n".upcase
+        print "\n Choose a Product to update: ".upcase
+        end
+        if update_productIds.empty?
+            print "This customer has no products. Would you like to add a product? Y/N: "
+            selection = gets.upcase.chomp
+           if selection == "Y"
+                self.add_product_to_active_customer
+                elsif selection =="N"
+            else
+                print "Unrecongnized selection".upcase
+                self.update_product
+           end
+        else
+            select_product_to_update = gets.chomp
+            if update_productIds.include?(select_product_to_update.to_i)
+                product_to_update = []
+                db = SQLite3::Database.open("bangazon_store.sqlite")
+                db_update_request = db.execute "SELECT * FROM products WHERE product_id = #{select_product_to_update} AND seller_id = #{$ACTIVE_CUSTOMER_ID}"
+                product_to_update = db_update_request
+                product_to_update.each do |product_id, price, title, description, quantity|
+                    
+                    print "\n\n1. Change title #{title}\n"
+                    print "2. Change description #{description}\n"
+                    print "3. Change price #{price}\n"
+                    print "4. Change quantity #{quantity}\n\n"
+                end
+                
+                print "\n\nSelect an option to update: "
+                update_selection = gets.chomp
+                
+                update_choice = case update_selection
+                
+                when 1
+                    update_title
+                when 2
+                    update_description
+                when 3
+                    update_price
+                when 4
+                    update_quantity
+                end
+
+            elsif select_product_to_update.upcase == "EXIT"
+    
+            else
+                print "Unrecongnized selection\n".upcase
+                    self.update_product
+            end
+        end
+    end
+
+    def update_title
+        print "Update Title"
+        new_title = gets.chomp
+    end
+
+    def update_description
+        print "Update Description"
+        new_description = gets.chomp
+    end
+
+    def update_price
+        print "Update Price"
+        new_price = gets.chomp.to_i
+    end
+
+    def update_quantity
+        print "Update Quantity"
+        new_quantity = gets.chomp
     end
 
     private
